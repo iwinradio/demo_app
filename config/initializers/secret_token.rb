314ae8995c95a547e0b0b47559a533bbc89a4d5c
Andrew Phung <iwinradio@gmail.com>
2011-11-29 11:30:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '37304df568bfb3c402be67285485adf5ad0febbce9df97c11009b044a71b27aac37620c1b836cae9b19c1d26ba2490f1471363249b8c008fa5389856811dde41'
